b9b8ead3681db09c1ef8b3fd57cb4a3c6c4b9e42
Anthony Fu <anthonyfu117@hotmail.com>
2016-08-12 23:07:23 +0800

    Support course drag and drop to rearrange

--- html/static/index.js
+++ html/static/index.js
@@ -3,7 +3,11 @@ if (!data)
 
 'use strict';
 
-GRADES = ['全年級','大一','大二','大三','大四','大五','大六'];
+var GRADES = ['全年級','大一','大二','大三','大四','大五','大六'];
+var COURSES;
+
+var selected_courses = [];
+var arrange_result = [];
 
 /* =============== Vue Directive ===============*/
 Vue.config.debug = true;
@@ -12,13 +16,13 @@ var vue_inst = new Vue({
   el: '#vue-root',
   data: {
     groups : data,
-    selected : [],
+    selected : selected_courses,
     GRADES: GRADES
   },
   methods: {
     is_selected: function(e){
-      for (var i=0;i<vue_inst.$data.selected.length;i++)
-        if (vue_inst.$data.selected[i].subject == e.subject)
+      for (var i=0;i<selected_courses.length;i++)
+        if (selected_courses[i].subject == e.subject)
           return true;
       return false;
     },
@@ -30,14 +34,53 @@ var vue_inst = new Vue({
     },
     add_course: function(e) {
       if (!vue_inst.is_selected(e))
-        vue_inst.$data.selected.push(e);
+        selected_courses.push(e);
     },
     remove_course: function(course) {
-      var index = vue_inst.$data.selected.indexOf(course);
-      vue_inst.$data.selected.splice(index, 1);
-    }
+      var index = selected_courses.indexOf(course);
+      selected_courses.splice(index, 1);
+    },
+    clear_selected: function() {
+      var len = selected_courses;
+      for (var i=0; i<len; i++)
+        selected_courses.pop();
+    },
+    handle_drop: function(itemOne, itemTwo) {
+      var dummy = selected_courses[itemOne.id];
+      selected_courses.$set(itemOne.id, selected_courses[itemTwo.id]);
+      selected_courses.$set(itemTwo.id, dummy);
+    },
   }
 });
+
+function get_all_course() {
+  if (COURSES) return COURSES;
+  COURSES = [];
+  $.each(data,function(_,group){
+    $.each(group[2],function(_,dept){
+      $.each(dept[2],function(_,course){
+        COURSES.push(course);
+      });
+    });
+  });
+  return COURSES;
+}
+function find_by_subject(subject)
+{
+  var r = [];
+  $.each(get_all_course(),function(_,e) {
+    if (e.subject == subject)
+      r.push(e);
+  });
+  return r;
+}
+function get_selected_subjects() {
+  var r = [];
+  $.each(selected_courses,function(_,e) {
+    r.push(e.subject);
+  });
+  return r;
+}
 function merge_array() {
   var result = []
   $.each(arguments, function(i,item){
