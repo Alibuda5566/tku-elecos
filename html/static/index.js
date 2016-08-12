d61d63a666e1b3c39feec0697cc372f2858fe6a1
Anthony Fu <anthonyfu117@hotmail.com>
2016-08-12 17:30:00 +0800

    Add course adding button

--- html/static/index.js
+++ html/static/index.js
@@ -3,6 +3,8 @@ if (!data)
 
 'use strict';
 
+GRADES = ['全年級','大一','大二','大三','大四','大五','大六'];
+
 /* =============== Vue Directive ===============*/
 Vue.config.debug = true;
 Vue.config.delimiters = ['${', '}'];
@@ -10,26 +12,25 @@ var vue_inst = new Vue({
   el: '#vue-root',
   data: {
     groups : data,
-    selected : []
+    selected : [],
+    GRADES: GRADES
   },
   methods: {
+    is_selected: function(e){
+      for (var i=0;i<vue_inst.$data.selected.length;i++)
+        if (vue_inst.$data.selected[i].subject == e.subject)
+          return true;
+      return false;
+    },
     add_dept: function (dept_data,term, obligatory) {
       $.each(dept_data,function(i,e) {
         if (e.term == term && (obligatory === undefined || e.obligatory == obligatory))
-        {
-          var selected = false;
-          for (var i=0;i<vue_inst.$data.selected.length;i++)
-          {
-            if (vue_inst.$data.selected[i].subject == e.subject)
-            {
-              selected = true;
-              break;
-            }
-          }
-          if (!selected)
-            vue_inst.$data.selected.push(e);
-        }
+          vue_inst.add_course(e);
       });
+    },
+    add_course: function(e) {
+      if (!vue_inst.is_selected(e))
+        vue_inst.$data.selected.push(e);
     },
     remove_course: function(course) {
       var index = vue_inst.$data.selected.indexOf(course);
