a0c3edfec2be8ada605937e34dae161f779938f3
Anthony Fu <anthonyfu117@hotmail.com>
2016-08-17 01:36:56 +0800

    Fix for windows unicode display

--- run.bat
+++ run.bat
@@ -1,8 +1,4 @@
-@REM @Author: Anthony
-@REM @Date:   2016-02-25 20:13:03
-@REM @Last Modified by:   Anthony
-@REM Modified time: 2016-03-23 02:38:53
 @echo off
-pip install -r pip-requirements.txt
+@chcp 65001
 python elecos.py
 pause
